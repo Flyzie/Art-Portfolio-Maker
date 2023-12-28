057dd61288db6aa649431e02aacc3ea2c9bc8604
Flyzie <bartirut@gmail.com>
2023-12-28 22:50:17 +0100

    Addedd div removal (basic)

--- main.js
+++ main.js
@@ -7,8 +7,12 @@
 
     const container = document.querySelector('.mainGrid');
     const popupAdd = document.querySelector('.popupAdd');
+    const piece = document.querySelector('.piece-1')
+
     const btn = document.querySelector('#btn');
     const btnFinalize = document.querySelector('#popupFinalize');
+    const removeBtn = document.querySelector('#btnRemove');
+
     const name = document.querySelector('#name');
     const dsc = document.querySelector('#description');
     const fileSelector = document.querySelector('#Artwork');
@@ -27,8 +31,10 @@
         const description = document.createElement('p');
         const image = document.createElement('img');
         const textBox = document.createElement('div');
-        const closeBtn = document.createElement('button');
+        const removeBtn = document.createElement('button');
         const closeIcon = document.createElement('img');
+
+        const uniqueId = generateId();
     
         content.classList.add('piece-1');
         content.style.gridColumnStart = colNrS;
@@ -36,8 +42,9 @@
         content.style.gridRowStart = rowNrS;
         content.style.gridRowEnd = rowNrE;
 
-        closeBtn.classList.add('closeBtn');
+        removeBtn.classList.add('btnRemove');
         closeIcon.src = "cross.png";
+        removeBtn.id = 'btnRemove';
 
         textBox.classList.add('textBox');
         
@@ -46,13 +53,20 @@
 
         image.setAttribute('src', URL.createObjectURL(fileSelector.files[0]));
 
-        closeBtn.appendChild(closeIcon);
+        removeBtn.appendChild(closeIcon);
         textBox.appendChild(label);
         textBox.appendChild(description);
         content.appendChild(image);
-        content.appendChild(closeBtn);
+        content.appendChild(removeBtn);
         content.appendChild(textBox);
-        
+
+        assignId(removeBtn, uniqueId);
+        assignId(content, uniqueId);
+
+        removeBtn.addEventListener('click', function() {
+            removeDiv(uniqueId);
+        });
+    
         container.appendChild(content);
 
         if(colNrS < 5){
@@ -68,9 +82,39 @@
         popupAdd.classList.remove('open');
     }
 
-    function removeDiv(){
-        
+    function removeDiv(id){
+        const divToRemove = document.querySelector('.piece-1[uid="' + id + '"]');
+        divToRemove.remove();
+
+        colNrS--;
+        colNrE--;
+        if(colNrS < 2){
+            rowNrS--;
+            rowNrE--;
+
+            colNrS = 5;
+            colNrE = 6;
+        }
     }
+
+    function generateId(){
+        return "id" + Math.random().toString(16).slice(2);
+    }
+
+    function assignId(element, id){
+        
+        element.setAttribute('uid', id);
+    }
+
+    removeBtn.addEventListener('click', ()=> {
+        if(removeBtn.getAttribute('uid') == piece.getAttribute('uid')){
+            removeDiv(removeBtn.getAttribute('uid'));
+        }else{
+            console.log('error');
+        }
+    });
+
+    
 
     btn.addEventListener('click', () => {
         popupAdd.classList.add('open');
