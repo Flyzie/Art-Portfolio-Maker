655fdf4b8271434b6b3cfbab75f38012f89a1cf9
Flyzie <bartirut@gmail.com>
2024-01-13 18:23:15 +0100

    Added option to inspect the artworks in full
    Page of inspected artwork loaded from a JSON file.

--- js/artworkCreation.js
+++ js/artworkCreation.js
@@ -1,4 +1,4 @@
-
+    import { loadArtwork } from './loadingArtwork.js';
     
     //const content = document.createElement('div');
     const label = document.createElement('h1');
@@ -18,10 +18,7 @@
     const dsc = document.querySelector('#description'); 
     const fileSelector = document.querySelector('#Artwork');
 
-    function removeDiv(id){
-        const divToRemove = document.querySelector('.piece-1[uid="' + id + '"]');
-        divToRemove.remove();
-    }
+    
 
     function generateId(){
         return "id" + Math.random().toString(16).slice(2);
@@ -30,55 +27,6 @@
     function assignId(element, id){
         
         element.setAttribute('uid', id);
-    }
-    
-    function createDiv(){
-        const content = document.createElement('div');
-        const label = document.createElement('h1');
-        const description = document.createElement('p');
-        const image = document.createElement('img');
-        const textBox = document.createElement('div');
-        const removeBtn = document.createElement('button');
-        const closeIcon = document.createElement('img');
-        const imageLinker = document.createElement('a')
-
-        const uniqueId = generateId();
-
-        imageLinker.id = 'image_linker';
-        imageLinker.href = 'artwork.html';
-    
-        content.classList.add('piece-1');
-
-        removeBtn.classList.add('btnRemove');
-        closeIcon.src = "cross.png";
-        removeBtn.id = 'btnRemove';
-
-        textBox.classList.add('textBox');
-        
-        label.innerHTML = document.getElementById('name').value;
-        description.innerHTML = document.getElementById('description').value;
-
-        image.setAttribute('src', URL.createObjectURL(fileSelector.files[0]));
-
-        removeBtn.appendChild(closeIcon);
-        textBox.appendChild(label);
-        textBox.appendChild(description);
-        imageLinker.appendChild(image);
-        content.appendChild(imageLinker);
-        content.appendChild(removeBtn);
-        content.appendChild(textBox);
-
-        assignId(removeBtn, uniqueId);
-        assignId(content, uniqueId);
-        
-
-        removeBtn.addEventListener('click', function() {
-            removeDiv(uniqueId);
-        });
-    
-        container.appendChild(content);
-
-        popupAdd.classList.remove('open');
     }
 
     function createDivFromData(data) {
@@ -92,11 +40,11 @@
         const imageLinker = document.createElement('a');
     
         imageLinker.id = 'image_linker';
-        imageLinker.href = 'artwork.html';
+        imageLinker.href = '/html/artwork.html';
     
         content.classList.add('piece-1');
         removeBtn.classList.add('btnRemove');
-        closeIcon.src = "cross.png";
+        closeIcon.src = "/img/cross.png";
         removeBtn.id = 'btnRemove';
         textBox.classList.add('textBox');
     
@@ -116,11 +64,37 @@
         assignId(content, data.uid);
     
         removeBtn.addEventListener('click', function () {
-            removeDiv(data.uid);
+            removeDivFromData(data.uid);
+        });
+
+        imageLinker.addEventListener('click', function(){
+            console.log('Artwork clicked');
+            imageLinker.href = '/html/artwork.html?uid=' + data.uid;
         });
     
         container.appendChild(content);
         popupAdd.classList.remove('open');
+    }
+
+    // Function to remove div from sessionStorage
+    function removeDivFromData(id) {
+        // Remove the div from the DOM
+        const divToRemove = document.querySelector('.piece-1[uid="' + id + '"]');
+        divToRemove.remove();
+
+        // Retrieve existing data from sessionStorage
+        const existingData = JSON.parse(sessionStorage.getItem('artworkData')) || [];
+
+        // Find the index of the data to be removed
+        const index = existingData.findIndex(data => data.uid === id);
+
+        // Remove the data from the existing data array
+        if (index !== -1) {
+            existingData.splice(index, 1);
+        }
+
+        // Save the updated data back to sessionStorage
+        sessionStorage.setItem('artworkData', JSON.stringify(existingData));
     }
     
     // Function to load data to sessionStorage
@@ -140,12 +114,12 @@
     
         // Save the updated data back to sessionStorage
         sessionStorage.setItem('artworkData', JSON.stringify(existingData));
-    
+
         // Call the function to create a div from the new data
         createDivFromData(newData);
     }
     
-    // ... (Your existing code)
+
     
     // Add this block of code to retrieve and display existing data during page load
     window.onload = function () {
@@ -155,7 +129,7 @@
         });
     };
 
-
+    /*
     removeBtn.addEventListener('click', ()=> {
         if(removeBtn.getAttribute('uid') == piece.getAttribute('uid')){
             removeDiv(removeBtn.getAttribute('uid'));
@@ -163,6 +137,7 @@
             console.log('error');
         }
     });
+    */
 
     btn.addEventListener('click', () => {
         popupAdd.classList.add('open');
---
+++ js/loadingArtwork.js
@@ -0,0 +1,31 @@
+
+function loadArtwork(id){
+        const artworkDisplay = document.querySelector('#artwork_display');
+        const image = artworkDisplay.firstElementChild;
+        const name = document.querySelector('#artwork_text > h1');
+        const description = document.querySelector('#artwork_text > p');
+
+        const existingData = JSON.parse(sessionStorage.getItem('artworkData')) || [];
+        const index = existingData.findIndex(data => data.uid === id);
+
+        if (index !== -1) {
+
+            const data = existingData[index];
+            console.log(data);
+            name.innerHTML = data.title;
+            description.innerHTML = data.description;
+            image.src = data.image;
+            
+        } else {
+
+            console.error('Artwork data not found.');
+        }
+}
+
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const uid = urlParams.get('uid');
+    if (uid) {
+        loadArtwork(uid);
+    }
+}
